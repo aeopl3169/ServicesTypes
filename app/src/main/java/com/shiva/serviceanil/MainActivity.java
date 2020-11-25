45b7c89bf76e6784f0a4dcbc9709d15b9a44886a
shashishiva9 <59079874+shashishiva9@users.noreply.github.com>
2020-11-25 11:40:07 +0530

    IntentService

--- app/src/main/java/com/shiva/serviceanil/MainActivity.java
+++ app/src/main/java/com/shiva/serviceanil/MainActivity.java
@@ -1,29 +1,16 @@
 package com.shiva.serviceanil;
 
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button buttonStart, buttonStop, buttonBind, buttonUnBind, buttonGetRandomNumber;
-    private TextView textViewThreadCount;
-    int count = 0;
-
-    private MyService myService;
-    private boolean isServiceBound; // Checking service is bound or unbound.
-    // ServiceConnection will be established on click of bindService button.
-    private ServiceConnection serviceConnection;
-
+    private Button buttonStart, buttonStop;
     private Intent serviceIntent;
     private boolean mStopLoop;
     static final String SERVICE = "Service";
@@ -36,80 +23,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonStart = (Button) findViewById(R.id.buttonStart);
         buttonStop = (Button) findViewById(R.id.buttonStop);
-        buttonBind = (Button) findViewById(R.id.buttonBind);
-        buttonUnBind = (Button) findViewById(R.id.buttonUnBind);
-        buttonGetRandomNumber = (Button) findViewById(R.id.buttonRandomNumber);
-        textViewThreadCount = (TextView) findViewById(R.id.textView);
-
         buttonStart.setOnClickListener(this);
         buttonStop.setOnClickListener(this);
-        buttonBind.setOnClickListener(this);
-        buttonUnBind.setOnClickListener(this);
-        buttonGetRandomNumber.setOnClickListener(this);
 
-        serviceIntent = new Intent(getApplicationContext(), MyService.class);
+        serviceIntent = new Intent(getApplicationContext(), MyIntentService.class);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonStart:
-                mStopLoop = true;
+//                mStopLoop = true;
                 startService(serviceIntent); // start service.
                 break;
             case R.id.buttonStop:
                 stopService(serviceIntent); // We can stop service explicitly with stopService.
                 break;
-            case R.id.buttonBind:
-                bindService();
-                break;
-            case R.id.buttonUnBind:
-                unbindService();
-                break;
-            case R.id.buttonRandomNumber:
-                setRandomNumber();
-                break;
             default:
                 throw new IllegalStateException("Unexpected value: " + v.getId());
-        }
-    }
-
-    private void bindService() {
-        if (serviceConnection == null) {
-            // initializing the ServiceConnection.
-            serviceConnection = new ServiceConnection() {
-                @Override
-                public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                    // Getting the service instance
-                    MyService.MyServiceBinder myServiceBinder = (MyService.MyServiceBinder) iBinder;
-                    myService = myServiceBinder.getService(); // Getting service instance
-                    isServiceBound = true;
-                }
-
-                @Override
-                public void onServiceDisconnected(ComponentName componentName) {
-                    isServiceBound = false;
-                }
-            };
-        }
-        // binding to the service
-        // BIND_AUTO_CREATE: If the service is not created It will create the service.
-        bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
-    }
-
-    private void unbindService() {
-        if (isServiceBound) {
-            Log.d(SERVICE, "unbindService: Called");
-            unbindService(serviceConnection);
-            isServiceBound = false;
-        }
-    }
-
-    private void setRandomNumber() {
-        if (isServiceBound) {
-            textViewThreadCount.setText("Random number: " + myService.getRandomNumber());
-        } else {
-            textViewThreadCount.setText("Service not bound");
         }
     }
 }
---
+++ app/src/main/java/com/shiva/serviceanil/MyIntentService.java
@@ -0,0 +1,65 @@
+package com.shiva.serviceanil;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.util.Random;
+
+import static com.shiva.serviceanil.MainActivity.SERVICE;
+
+/**
+ * E/ThrottleService: problem during onPollAlarm: java.lang.IllegalStateException: problem parsing stats:
+ * java.io.FileNotFoundException: /proc/net/xt_qtaguid/iface_stat_all: open failed: ENOENT (No such file or directory)
+ */
+public class MyIntentService extends IntentService {
+
+    private int mRandomNumber;
+    private boolean mIsRandomGeneratorOn;
+    private final int MIN = 0;
+    private final int MAX = 100;
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @ name Used to name the worker thread, important only for debugging.
+     */
+//    public MyIntentService(String name) {
+//        super(name);
+//    }
+    public MyIntentService() {
+        super(MyIntentService.class.getSimpleName());
+    }
+
+    // Compulsory override method.
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        mIsRandomGeneratorOn = true;
+        startRandomNumberGenerator();
+    }
+
+    private void startRandomNumberGenerator() {
+        while (mIsRandomGeneratorOn) { // If true then generates random number
+            try {
+                Thread.sleep(1000);
+                if (mIsRandomGeneratorOn) {
+                    mRandomNumber = new Random().nextInt(MAX) + MIN;
+                    Log.i(SERVICE, "stopRandomNumberGenerator: Thread id: " + Thread.currentThread().getId() + " Random number: " + mRandomNumber);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Log.i(SERVICE, "startRandomNumberGenerator: Thread interrupted.");
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mIsRandomGeneratorOn = false;
+        Log.d(SERVICE, "onDestroy: IntentService destroyed on Thread id: "+Thread.currentThread().getId());
+    }
+}
