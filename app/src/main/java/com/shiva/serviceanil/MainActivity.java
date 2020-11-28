b87c5bff80b2ddc68c5bdfae30ef0c22292a0094
shashishiva9 <59079874+shashishiva9@users.noreply.github.com>
2020-11-28 16:52:04 +0530

    17-18 JObScheduler

--- app/src/main/java/com/shiva/serviceanil/MainActivity.java
+++ app/src/main/java/com/shiva/serviceanil/MainActivity.java
@@ -1,59 +1,81 @@
 package com.shiva.serviceanil;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
 import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonStart, buttonStop;
-    int count = 0;
 
-    private MyIntentService myIntentService;
-    private boolean isServiceBound; // Checking service is bound or unbound.
-    // ServiceConnection will be established on click of bindService button.
-    private ServiceConnection serviceConnection;
+    JobScheduler jobScheduler;
 
-    private Intent serviceIntent;
-    private boolean mStopLoop;
     static final String SERVICE = "Service";
 
+    //    @SuppressLint("NewApi")
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(SERVICE, "onCreate: thread id: " + Thread.currentThread().getId());
 
-        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStart = findViewById(R.id.buttonStart);
         buttonStop = (Button) findViewById(R.id.buttonStop);
 
         buttonStart.setOnClickListener(this);
         buttonStop.setOnClickListener(this);
-
-        serviceIntent = new Intent(getApplicationContext(), MyIntentService.class);
+        // If we are running on the lower version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonStart:
-                serviceIntent.putExtra("starter", "starter" + (++count));
-                MyIntentService.enqueueWork(this, serviceIntent); // To start JobIntentService
+                startJob();
                 break;
             case R.id.buttonStop:
-                stopService(serviceIntent); // We can stop service explicitly with stopService.
+                stopJob();
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void startJob() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ComponentName componentName = new ComponentName(this, MyJobService.class);
+            JobInfo jobInfo = new JobInfo.Builder(101, componentName)
+                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_CELLULAR)
+                    .setPeriodic(15 * 60 * 1000) // It should be greater than 15 minutes
+                    .setRequiresCharging(true)
+                    .setPersisted(true)
+                    .build();
+            if (jobScheduler.schedule(jobInfo) == JobScheduler.RESULT_SUCCESS) {
+                Log.i(SERVICE, "startJob: Thread id: " + Thread.currentThread().getId() + " job scheduled successfully.");
+            } else {
+                Log.i(SERVICE, "startJob: Thread id: " + Thread.currentThread().getId() + " job could not schedule.");
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void stopJob() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            jobScheduler.cancel(101);
         }
     }
 }
